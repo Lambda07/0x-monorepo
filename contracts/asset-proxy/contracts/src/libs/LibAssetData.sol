bca69cabd60f95bf60067a3fb3434205f228bd41
F. Eugene Aumson <feuGeneA@users.noreply.github.com>
2019-04-08 19:59:59 -0400

    Add LibAssetData contract

---
+++ contracts/asset-proxy/contracts/src/libs/LibAssetData.sol
@@ -0,0 +1,225 @@
+// TODO: change all contract references to interface references
+/*
+
+  Copyright 2019 ZeroEx Intl.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+pragma solidity ^0.5.5;
+pragma experimental ABIEncoderV2;
+
+import "@0x/contracts-utils/contracts/src/LibBytes.sol";
+import "@0x/contracts-erc1155/contracts/src/interfaces/IERC1155Mintable.sol";
+import "@0x/contracts-erc20/contracts/src/interfaces/IERC20Token.sol";
+import "@0x/contracts-erc721/contracts/src/interfaces/IERC721Token.sol";
+
+import "../ERC20Proxy.sol";
+import "../ERC721Proxy.sol";
+
+
+library LibAssetData {
+    bytes4 constant public ERC20_PROXY_ID = bytes4(keccak256("ERC20Token(address)"));
+    bytes4 constant public ERC721_PROXY_ID = bytes4(keccak256("ERC721Token(address,uint256)"));
+    bytes4 constant public MULTI_ASSET_PROXY_ID = bytes4(keccak256("MultiAsset(uint256[],bytes[])"));
+    bytes4 constant public ERC1155_PROXY_ID = bytes4(keccak256("ERC1155Assets(address,uint256[],uint256[],bytes)"));
+
+    function balanceOf(address owner, bytes memory assetData)
+        public
+        view
+        returns (uint256 amount)
+    {
+        bytes4 proxyId = LibBytes.readBytes4(assetData, 0);
+
+        if (proxyId == ERC20_PROXY_ID) {
+            return IERC20Token(LibBytes.readAddress(assetData, 16)).balanceOf(owner);
+        } else if (proxyId == ERC721_PROXY_ID) {
+            return IERC721Token(LibBytes.readAddress(assetData, 16)).balanceOf(owner);
+        } else if (proxyId == ERC1155_PROXY_ID) {
+            // solhint-disable-next-line
+            (address tokenAddress, uint256[] memory tokenIds, , ) = abi.decode(
+                LibBytes.slice(assetData, 4, assetData.length),
+                // solhint-disable-next-line
+                (address, uint256[], uint256[], bytes)
+            );
+            address[] memory owners = new address[](1);
+            owners[0] = owner;
+            return IERC1155Mintable(tokenAddress).balanceOfBatch(owners, tokenIds)[0];
+        } else if (proxyId == MULTI_ASSET_PROXY_ID) {
+            // solhint-disable-next-line
+            (uint256[] memory assetAmounts, bytes[] memory nestedAssetData) = abi.decode(
+                // solhint-disable-next-line
+                LibBytes.slice(assetData, 4, assetData.length), (uint256[], bytes[])
+            );
+            uint256 lowestAssetBalance = ~uint256(0);
+            for (uint256 i = 0; i < nestedAssetData.length; i++) {
+                uint256 assetBalance = balanceOf(owner, nestedAssetData[i]) / assetAmounts[i];
+                if (assetBalance < lowestAssetBalance) {
+                    lowestAssetBalance = assetBalance;
+                }
+            }
+            return lowestAssetBalance;
+        } else {
+            revert("Unsupported proxy identifier");
+        }
+    }
+
+    function allowance(address owner, address spender, bytes memory assetData)
+        // should we rename to getApproval()? "Allowance" seems out of vogue.
+        public
+        view
+        returns (uint256 amount)
+    {
+        bytes4 proxyId = LibBytes.readBytes4(assetData, 0);
+
+        if (proxyId == ERC20_PROXY_ID) {
+            return IERC20Token(LibBytes.readAddress(assetData, 16)).allowance(owner, spender);
+        } else if (proxyId == ERC721_PROXY_ID) {
+            (address tokenAddress, uint256 tokenId) = abi.decode(
+                LibBytes.slice(assetData, 4, assetData.length),
+                (address, uint256)
+            );
+            if (spender == IERC721Token(tokenAddress).getApproved(tokenId)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else if (proxyId == ERC1155_PROXY_ID) {
+            // solhint-disable-next-line
+            (address tokenAddress, , , ) = abi.decode(
+                LibBytes.slice(assetData, 4, assetData.length),
+                // solhint-disable-next-line
+                (address, uint256[], uint256[], bytes)
+            );
+            if (IERC1155(tokenAddress).isApprovedForAll(owner, spender)) {
+                return 1;
+            } else {
+                return 0;
+            }
+        //} else if (proxyId == MULTI_ASSET_PROXY_ID) {
+        } else {
+            revert("Unsupported proxy identifier");
+        }
+    }
+
+    function encodeERC20AssetData(address tokenAddress)
+        public
+        pure
+        returns (bytes memory assetData)
+    {
+        assetData = abi.encodeWithSelector(ERC20_PROXY_ID, tokenAddress);
+    }
+
+    function decodeERC20AssetData(bytes memory assetData)
+        public
+        pure
+        returns (
+            bytes4 proxyId,
+            address tokenAddress
+        )
+    {
+        proxyId = LibBytes.readBytes4(assetData, 0);
+
+        require(proxyId == ERC20_PROXY_ID);
+
+        tokenAddress = LibBytes.readAddress(assetData, 16);
+    }
+
+    function encodeERC721AssetData(
+        address tokenAddress,
+        uint256 tokenId
+    )
+        public
+        pure
+        returns (bytes memory assetData)
+    {
+        assetData = abi.encodeWithSelector(ERC721_PROXY_ID, tokenAddress, tokenId);
+    }
+
+    function decodeERC721AssetData(bytes memory assetData)
+        public
+        pure
+        returns (
+            bytes4 proxyId,
+            address tokenAddress,
+            uint256 tokenId
+        )
+    {
+        proxyId = LibBytes.readBytes4(assetData, 0);
+
+        require(proxyId == ERC721_PROXY_ID);
+
+        tokenAddress = LibBytes.readAddress(assetData, 16);
+        tokenId = LibBytes.readUint256(assetData, 36);
+    }
+
+    function encodeMultiAssetData(uint256[] memory amounts, bytes[] memory nestedAssetData)
+        public
+        pure
+        returns (bytes memory assetData)
+    {
+        assetData = abi.encodeWithSelector(MULTI_ASSET_PROXY_ID, amounts, nestedAssetData);
+    }
+
+    function decodeMultiAssetData(bytes memory assetData)
+        public
+        pure
+        returns (
+            bytes4 proxyId,
+            uint256[] memory amounts,
+            bytes[] memory nestedAssetData
+        )
+    {
+        proxyId = LibBytes.readBytes4(assetData, 0);
+
+        require(proxyId == MULTI_ASSET_PROXY_ID);
+
+        // solhint-disable-next-line
+        (amounts, nestedAssetData) = abi.decode(LibBytes.slice(assetData, 4, assetData.length), (uint256[], bytes[]));
+    }
+
+    function encodeERC1155AssetData(
+        address tokenAddress,
+        uint256[] memory tokenIds,
+        uint256[] memory tokenValues,
+        bytes memory callbackData
+    )
+        public
+        pure
+        returns (bytes memory assetData)
+    {
+        assetData = abi.encodeWithSelector(ERC1155_PROXY_ID, tokenAddress, tokenIds, tokenValues, callbackData);
+    }
+
+    function decodeERC1155AssetData(bytes memory assetData)
+        public
+        pure
+        returns (
+            bytes4 proxyId,
+            address tokenAddress,
+            uint256[] memory tokenIds,
+            uint256[] memory tokenValues,
+            bytes memory callbackData
+        )
+    {
+        proxyId = LibBytes.readBytes4(assetData, 0);
+
+        require(proxyId == ERC1155_PROXY_ID);
+
+        // solhint-disable-next-line
+        (tokenAddress, tokenIds, tokenValues, callbackData) = abi.decode(
+            LibBytes.slice(assetData, 4, assetData.length), (address, uint256[], uint256[], bytes)
+        );
+    }
+}
---
+++ contracts/asset-proxy/test/lib_asset_data.ts
@@ -0,0 +1,305 @@
+import * as chai from 'chai';
+import { LogWithDecodedArgs } from 'ethereum-types';
+
+import {
+    artifacts as erc1155Artifacts,
+    ERC1155MintableContract,
+    ERC1155TransferSingleEventArgs,
+    IERC1155MintableContract,
+} from '@0x/contracts-erc1155';
+import { artifacts as erc20Artifacts, DummyERC20TokenContract, IERC20TokenContract } from '@0x/contracts-erc20';
+import { artifacts as erc721Artifacts, DummyERC721TokenContract, IERC721TokenContract } from '@0x/contracts-erc721';
+import { chaiSetup, constants, LogDecoder, provider, txDefaults, web3Wrapper } from '@0x/contracts-test-utils';
+import { BlockchainLifecycle } from '@0x/dev-utils';
+import { AssetProxyId } from '@0x/types';
+import { BigNumber } from '@0x/utils';
+
+import { artifacts, LibAssetDataContract } from '../src';
+
+chaiSetup.configure();
+const expect = chai.expect;
+
+const blockchainLifecycle = new BlockchainLifecycle(web3Wrapper);
+
+const KNOWN_ERC20_ENCODING = {
+    address: '0x1dc4c1cefef38a777b15aa20260a54e584b16c48',
+    assetData: '0xf47261b00000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48',
+};
+const KNOWN_ERC721_ENCODING = {
+    address: '0x1dc4c1cefef38a777b15aa20260a54e584b16c48',
+    tokenId: new BigNumber(1),
+    assetData:
+        '0x025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000001',
+};
+const KNOWN_ERC1155_ENCODING = {
+    tokenAddress: '0x1dc4c1cefef38a777b15aa20260a54e584b16c48',
+    tokenIds: [new BigNumber(100), new BigNumber(1001), new BigNumber(10001)],
+    tokenValues: [new BigNumber(200), new BigNumber(2001), new BigNumber(20001)],
+    callbackData:
+        '0x025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000001',
+    assetData:
+        '0xa7cb5fb70000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001800000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000006400000000000000000000000000000000000000000000000000000000000003e90000000000000000000000000000000000000000000000000000000000002711000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000c800000000000000000000000000000000000000000000000000000000000007d10000000000000000000000000000000000000000000000000000000000004e210000000000000000000000000000000000000000000000000000000000000044025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000',
+};
+const KNOWN_MULTI_ASSET_ENCODING = {
+    amounts: [new BigNumber(70), new BigNumber(1), new BigNumber(18)],
+    nestedAssetData: [
+        KNOWN_ERC20_ENCODING.assetData,
+        KNOWN_ERC721_ENCODING.assetData,
+        KNOWN_ERC1155_ENCODING.assetData,
+    ],
+    assetData:
+        '0x94cfcdd7000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000046000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000120000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000000c000000000000000000000000000000000000000000000000000000000000001400000000000000000000000000000000000000000000000000000000000000024f47261b00000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c48000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000204a7cb5fb70000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c480000000000000000000000000000000000000000000000000000000000000080000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001800000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000006400000000000000000000000000000000000000000000000000000000000003e90000000000000000000000000000000000000000000000000000000000002711000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000c800000000000000000000000000000000000000000000000000000000000007d10000000000000000000000000000000000000000000000000000000000004e210000000000000000000000000000000000000000000000000000000000000044025717920000000000000000000000001dc4c1cefef38a777b15aa20260a54e584b16c4800000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+};
+
+describe('LibAssetData', () => {
+    let libAssetData: LibAssetDataContract;
+
+    let tokenOwnerAddress: string;
+    let approvedSpenderAddress: string;
+
+    let erc20TokenAddress: string;
+    const erc20TokenTotalSupply = new BigNumber(1);
+
+    let erc721TokenAddress: string;
+    const firstERC721TokenId = new BigNumber(1);
+    const numberOfERC721Tokens = 10;
+
+    let erc1155MintableAddress: string;
+    let erc1155TokenId: BigNumber;
+
+    before(async () => {
+        await blockchainLifecycle.startAsync();
+        libAssetData = await LibAssetDataContract.deployFrom0xArtifactAsync(
+            artifacts.LibAssetData,
+            provider,
+            txDefaults,
+        );
+
+        tokenOwnerAddress = (await web3Wrapper.getAvailableAddressesAsync())[0];
+        approvedSpenderAddress = (await web3Wrapper.getAvailableAddressesAsync())[1];
+
+        erc20TokenAddress = (await DummyERC20TokenContract.deployFrom0xArtifactAsync(
+            erc20Artifacts.DummyERC20Token,
+            provider,
+            txDefaults,
+            'Dummy',
+            'DUM',
+            new BigNumber(1),
+            erc20TokenTotalSupply,
+        )).address;
+
+        const erc721TokenContract = await DummyERC721TokenContract.deployFrom0xArtifactAsync(
+            erc721Artifacts.DummyERC721Token,
+            provider,
+            txDefaults,
+            'Dummy',
+            'DUM',
+        );
+        erc721TokenAddress = erc721TokenContract.address;
+        const transactionMinedPromises = [];
+        for (let i = 0; i < numberOfERC721Tokens; i++) {
+            transactionMinedPromises.push(
+                web3Wrapper.awaitTransactionSuccessAsync(
+                    await erc721TokenContract.mint.sendTransactionAsync(
+                        tokenOwnerAddress,
+                        firstERC721TokenId.plus(i - 1),
+                    ),
+                    constants.AWAIT_TRANSACTION_MINED_MS,
+                ),
+            );
+        }
+        await Promise.all(transactionMinedPromises);
+
+        const erc1155MintableContract = await ERC1155MintableContract.deployFrom0xArtifactAsync(
+            erc1155Artifacts.ERC1155Mintable,
+            provider,
+            txDefaults,
+        );
+        erc1155MintableAddress = erc1155MintableContract.address;
+        // Somewhat re-inventing the wheel here, but the prior art currently
+        // exists only as an unexported test util in the erc1155 package
+        // (Erc1155Wrapper.mintFungibleTokensAsync() in erc1155/test/utils/).
+        // This is concise enough to justify duplication, but it sure is ugly.
+        // tslint:disable-next-line no-unnecessary-type-assertion
+        erc1155TokenId = ((await new LogDecoder(web3Wrapper, erc1155Artifacts).getTxWithDecodedLogsAsync(
+            await erc1155MintableContract.create.sendTransactionAsync('uri:Dummy', /*isNonFungible:*/ false),
+        )).logs[0] as LogWithDecodedArgs<ERC1155TransferSingleEventArgs>).args.id;
+        await web3Wrapper.awaitTransactionSuccessAsync(
+            await erc1155MintableContract.mintFungible.sendTransactionAsync(
+                erc1155TokenId,
+                [tokenOwnerAddress],
+                [new BigNumber(1)],
+            ),
+            constants.AWAIT_TRANSACTION_MINED_MS,
+        );
+    });
+
+    after(async () => {
+        await blockchainLifecycle.revertAsync();
+    });
+
+    it('should encode ERC20 asset data', async () => {
+        expect(await libAssetData.encodeERC20AssetData.callAsync(KNOWN_ERC20_ENCODING.address)).to.equal(
+            KNOWN_ERC20_ENCODING.assetData,
+        );
+    });
+
+    it('should decode ERC20 asset data', async () => {
+        expect(await libAssetData.decodeERC20AssetData.callAsync(KNOWN_ERC20_ENCODING.assetData)).to.deep.equal([
+            AssetProxyId.ERC20,
+            KNOWN_ERC20_ENCODING.address,
+        ]);
+    });
+
+    it('should encode ERC721 asset data', async () => {
+        expect(
+            await libAssetData.encodeERC721AssetData.callAsync(
+                KNOWN_ERC721_ENCODING.address,
+                KNOWN_ERC721_ENCODING.tokenId,
+            ),
+        ).to.equal(KNOWN_ERC721_ENCODING.assetData);
+    });
+
+    it('should decode ERC721 asset data', async () => {
+        expect(await libAssetData.decodeERC721AssetData.callAsync(KNOWN_ERC721_ENCODING.assetData)).to.deep.equal([
+            AssetProxyId.ERC721,
+            KNOWN_ERC721_ENCODING.address,
+            KNOWN_ERC721_ENCODING.tokenId,
+        ]);
+    });
+
+    it('should encode multiasset data', async () => {
+        expect(
+            await libAssetData.encodeMultiAssetData.callAsync(
+                KNOWN_MULTI_ASSET_ENCODING.amounts,
+                KNOWN_MULTI_ASSET_ENCODING.nestedAssetData,
+            ),
+        ).to.equal(KNOWN_MULTI_ASSET_ENCODING.assetData);
+    });
+
+    it('should decode multiasset data', async () => {
+        expect(await libAssetData.decodeMultiAssetData.callAsync(KNOWN_MULTI_ASSET_ENCODING.assetData)).to.deep.equal([
+            AssetProxyId.MultiAsset,
+            KNOWN_MULTI_ASSET_ENCODING.amounts,
+            KNOWN_MULTI_ASSET_ENCODING.nestedAssetData,
+        ]);
+    });
+
+    it('should encode ERC1155 asset data', async () => {
+        expect(
+            await libAssetData.encodeERC1155AssetData.callAsync(
+                KNOWN_ERC1155_ENCODING.tokenAddress,
+                KNOWN_ERC1155_ENCODING.tokenIds,
+                KNOWN_ERC1155_ENCODING.tokenValues,
+                KNOWN_ERC1155_ENCODING.callbackData,
+            ),
+        ).to.equal(KNOWN_ERC1155_ENCODING.assetData);
+    });
+
+    it('should decode ERC1155 asset data', async () => {
+        expect(await libAssetData.decodeERC1155AssetData.callAsync(KNOWN_ERC1155_ENCODING.assetData)).to.deep.equal([
+            AssetProxyId.ERC1155,
+            KNOWN_ERC1155_ENCODING.tokenAddress,
+            KNOWN_ERC1155_ENCODING.tokenIds,
+            KNOWN_ERC1155_ENCODING.tokenValues,
+            KNOWN_ERC1155_ENCODING.callbackData,
+        ]);
+    });
+
+    it('should query ERC20 balance', async () => {
+        expect(
+            await libAssetData.balanceOf.callAsync(
+                tokenOwnerAddress,
+                await libAssetData.encodeERC20AssetData.callAsync(erc20TokenAddress),
+            ),
+        ).to.bignumber.equal(erc20TokenTotalSupply);
+    });
+
+    it('should query ERC721 balance', async () => {
+        expect(
+            await libAssetData.balanceOf.callAsync(
+                tokenOwnerAddress,
+                await libAssetData.encodeERC721AssetData.callAsync(erc721TokenAddress, firstERC721TokenId),
+            ),
+        ).to.bignumber.equal(numberOfERC721Tokens);
+    });
+
+    it('should query ERC1155 balanceOfBatch', async () => {
+        expect(
+            await libAssetData.balanceOf.callAsync(
+                tokenOwnerAddress,
+                await libAssetData.encodeERC1155AssetData.callAsync(
+                    erc1155MintableAddress,
+                    [erc1155TokenId],
+                    [new BigNumber(1)], // token values
+                    '0x', // callback data
+                ),
+            ),
+        ).to.bignumber.equal(1);
+    });
+
+    it('should query multi-asset batch balance', async () => {
+        expect(
+            await libAssetData.balanceOf.callAsync(
+                tokenOwnerAddress,
+                await libAssetData.encodeMultiAssetData.callAsync(
+                    [new BigNumber(1), new BigNumber(1)],
+                    [
+                        await libAssetData.encodeERC20AssetData.callAsync(erc20TokenAddress),
+                        await libAssetData.encodeERC721AssetData.callAsync(erc721TokenAddress, firstERC721TokenId),
+                    ],
+                ),
+            ),
+        ).to.bignumber.equal(Math.min(erc20TokenTotalSupply.toNumber(), numberOfERC721Tokens));
+    });
+
+    it('should query ERC20 allowances', async () => {
+        new IERC20TokenContract(
+            erc20Artifacts.IERC20Token.compilerOutput.abi,
+            erc20TokenAddress,
+            provider,
+        ).approve.sendTransactionAsync(approvedSpenderAddress, new BigNumber(1), { from: tokenOwnerAddress });
+        expect(
+            await libAssetData.allowance.callAsync(
+                tokenOwnerAddress,
+                approvedSpenderAddress,
+                await libAssetData.encodeERC20AssetData.callAsync(erc20TokenAddress),
+            ),
+        ).to.bignumber.equal(1);
+    });
+
+    it('should query ERC721 allowances', async () => {
+        new IERC721TokenContract(
+            erc721Artifacts.IERC721Token.compilerOutput.abi,
+            erc721TokenAddress,
+            provider,
+        ).approve.sendTransactionAsync(approvedSpenderAddress, new BigNumber(1), { from: tokenOwnerAddress });
+        expect(
+            await libAssetData.allowance.callAsync(
+                tokenOwnerAddress,
+                approvedSpenderAddress,
+                await libAssetData.encodeERC721AssetData.callAsync(erc721TokenAddress, firstERC721TokenId),
+            ),
+        ).to.bignumber.equal(1);
+    });
+
+    it('should query ERC1155 allowances', async () => {
+        new IERC1155MintableContract(
+            erc1155Artifacts.IERC1155Mintable.compilerOutput.abi,
+            erc1155MintableAddress,
+            provider,
+        ).setApprovalForAll.sendTransactionAsync(approvedSpenderAddress, true, { from: tokenOwnerAddress });
+        expect(
+            await libAssetData.allowance.callAsync(
+                tokenOwnerAddress,
+                approvedSpenderAddress,
+                await libAssetData.encodeERC1155AssetData.callAsync(
+                    erc1155MintableAddress,
+                    [erc1155TokenId],
+                    [new BigNumber(1)],
+                    '0x',
+                ),
+            ),
+        ).to.bignumber.equal(1);
+    });
+});
